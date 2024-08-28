d2762ed9d07e717671a6ce29ef823c3bad9c5e0f
desmond-hui <desmond@flipsidecryto.com>
2024-08-28 06:59:19 -0700

    replace xyz with eclipse

--- macros/create_udfs.sql
+++ macros/create_udfs.sql
@@ -1,2 +1,10 @@
 {% macro create_udfs() %}
+    {% if var("UPDATE_UDFS_AND_SPS") %}
+        {% set sql %}
+        {% if target.database != "ECLIPSE_COMMUNITY_DEV" %}
+            {{ create_udf_bulk_rest_api_v2() }};
+        {% endif %}
+        {% endset %}
+        {% do run_query(sql) %}
+    {% endif %}
 {% endmacro %}
