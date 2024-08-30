9319a5b0a6749a28eff3af9c484d211aa4988831
desmond-hui <desmond@flipsidecryto.com>
2024-08-30 09:46:51 -0700

    block txs

---
+++ models/bronze/streamline/core/bronze__transactions.sql
@@ -0,0 +1,12 @@
+{{ config (
+    materialized = 'view'
+) }}
+
+{% set model = "block_txs" %}
+{{ streamline_external_table_query_v2(
+    model,
+    partition_function = "CAST(SPLIT_PART(SPLIT_PART(file_name, '/', 3), '_', 1) AS INTEGER)",
+    partition_name = "_partition_by_block_id",
+    unique_key = "block_id",
+    other_cols="data:error::variant AS error, data:transaction:signatures[0]::string AS tx_id, value:array_index AS index"
+) }}
---
+++ models/streamline/core/complete/streamline__block_txs_complete.sql
@@ -0,0 +1,33 @@
+-- depends_on: {{ ref('bronze__transactions') }}
+-- depends_on: {{ ref('bronze__FR_transactions') }}
+
+{{ config (
+    materialized = "incremental",
+    unique_key = 'block_id',
+    merge_exclude_columns = ["inserted_timestamp"],
+    cluster_by = "ROUND(block_id, -5)",
+) }}
+
+SELECT
+    block_id,
+    error,
+    _partition_by_block_id,
+    _inserted_timestamp,
+    sysdate() AS inserted_timestamp,
+    sysdate() AS modified_timestamp,
+    '{{ invocation_id }}' AS _invocation_id,
+FROM
+{% if is_incremental() %}
+    {{ ref('bronze__transactions') }}
+WHERE
+    _inserted_timestamp >= (
+        SELECT
+            COALESCE(MAX(_INSERTED_TIMESTAMP), '1970-01-01' :: DATE) max_INSERTED_TIMESTAMP
+        FROM
+            {{ this }}
+    )
+{% else %}
+    {{ ref('bronze__FR_transactions') }}
+{% endif %}
+QUALIFY
+    row_number() OVER (PARTITION BY block_id ORDER BY _inserted_timestamp DESC) = 1
