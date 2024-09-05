5a3cb215c0fd1ba12aa032d890fe4967fa9a3ca7
desmond-hui <desmond@flipsidecryto.com>
2024-09-05 07:34:00 -0700

    remove block_id from cluster keys

--- models/silver/core/silver__transactions.sql
+++ models/silver/core/silver__transactions.sql
@@ -2,7 +2,7 @@
     materialized = 'incremental',
     unique_key = "tx_id",
     incremental_predicates = ["dynamic_range_predicate", "block_timestamp::date"],
-    cluster_by = ['block_timestamp::DATE','block_id','_inserted_timestamp::DATE'],
+    cluster_by = ['block_timestamp::DATE','_inserted_timestamp::DATE'],
     merge_exclude_columns = ["inserted_timestamp"],
     tags = ['scheduled_core']
 ) }}
