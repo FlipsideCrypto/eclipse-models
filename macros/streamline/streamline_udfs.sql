85e30f018252bb7ea397a78a73d56bba32ef7787
desmond-hui <desmond@flipsidecryto.com>
2024-09-11 07:25:06 -0700

    add prod integration

--- macros/streamline/streamline_udfs.sql
+++ macros/streamline/streamline_udfs.sql
@@ -2,10 +2,13 @@
     CREATE
     OR REPLACE EXTERNAL FUNCTION streamline.udf_bulk_rest_api_v2(json OBJECT) 
     returns ARRAY 
-    api_integration = AWS_ECLIPSE_API_STG_V2 
-    AS {% if target.database == 'ECLIPSE' -%}
-        'https://m4liqml8ch.execute-api.us-east-1.amazonaws.com/stg/udf_bulk_rest_api' /* TODO: update when prod is deployed */
+    {% if target.database == 'ECLIPSE' -%}
+    api_integration = AWS_ECLIPSE_API_PROD
+    AS
+        'https://8zxk0vgb25.execute-api.us-east-1.amazonaws.com/stg/udf_bulk_rest_api' /* TODO: update when prod is deployed */
     {% else %}
+    api_integration = AWS_ECLIPSE_API_STG_V2 
+    AS
         'https://m4liqml8ch.execute-api.us-east-1.amazonaws.com/stg/udf_bulk_rest_api'
     {%- endif %}
 {% endmacro %}
