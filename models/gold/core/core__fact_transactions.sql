8210a4fbd7e23b86f258ed0251f79a2fb5a52914
desmond-hui <desmond@flipsidecryto.com>
2024-09-16 07:52:20 -0700

    core transactions

---
+++ models/gold/core/core__fact_transactions.sql
@@ -0,0 +1,53 @@
+-- depends_on: {{ ref('bronze__blocks') }}
+
+{{ config(
+    materialized = 'incremental',
+    unique_key = "tx_id",
+    incremental_predicates = ["dynamic_range_predicate", "block_timestamp::date"],
+    cluster_by = ['block_timestamp::DATE'],
+    tags = ['scheduled_core']
+) }}
+
+{% if execute %}
+    {% if is_incremental() %}
+    {% set max_modified_query %}
+    SELECT
+        MAX(modified_timestamp) AS modified_timestamp
+    FROM
+        {{ this }}
+    {% endset %}
+    {% set max_modified_timestamp = run_query(max_modified_query)[0][0] %}
+    {% endif %}
+{% endif %}
+
+SELECT
+    block_timestamp,
+    block_id,
+    tx_id,
+    index,
+    recent_block_hash,
+    signers,
+    fee,
+    succeeded,
+    account_keys,
+    pre_balances,
+    post_balances,
+    pre_token_balances,
+    post_token_balances,
+    instructions,
+    inner_instructions,
+    log_messages,
+    address_table_lookups,
+    units_consumed,
+    units_limit,
+    tx_size,
+    version,
+    transactions_id AS fact_transactions_id,
+    inserted_timestamp,
+    modified_timestamp
+FROM
+    {{ ref('silver__transactions') }}
+{% if is_incremental() %}
+WHERE
+    modified_timestamp > (SELECT max(modified_timestamp) FROM {{ this }})
+{% endif %}
