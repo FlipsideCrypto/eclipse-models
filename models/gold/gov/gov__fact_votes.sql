848ddc046b8c68db3027f030824e01185b7f1453
Eric Laurello <eric.laurello@flipsidecrypto.com>
2024-09-18 16:10:45 -0400

    overview and fact votes

--- models/gold/gov/gov__fact_votes.sql
+++ models/gold/gov/gov__fact_votes.sql
@@ -4,28 +4,33 @@
     incremental_predicates = ["dynamic_range_predicate", "block_timestamp::date"],
     merge_exclude_columns = ["inserted_timestamp"],
     cluster_by = ['block_timestamp::DATE'],
-    post_hook = enable_search_optimization('{{this.schema}}','{{this.identifier}}','ON EQUALITY(tx_id)'),
+    post_hook = enable_search_optimization(
+        '{{this.schema}}',
+        '{{this.identifier}}',
+        'ON EQUALITY(tx_id)'
+    ),
     tags = ['scheduled_core']
 ) }}
 
-
 {% if execute %}
-    {% if is_incremental() %}
-    {% set max_modified_query %}
-    SELECT
-        MAX(modified_timestamp) AS modified_timestamp
-    FROM
-        {{ this }}
-    {% endset %}
-    {% set max_modified_timestamp = run_query(max_modified_query)[0][0] %}
-    {% endif %}
-{% endif %}
 
+{% if is_incremental() %}
+{% set max_modified_query %}
+
+SELECT
+    MAX(modified_timestamp) AS modified_timestamp
+FROM
+    {{ this }}
+
+    {% endset %}
+    {% set max_modified_timestamp = run_query(max_modified_query) [0] [0] %}
+{% endif %}
+{% endif %}
 SELECT
     block_timestamp,
     block_id,
     tx_id,
-    index,
+    INDEX,
     recent_block_hash,
     signers,
     fee,
@@ -36,12 +41,13 @@ SELECT
     instruction,
     version,
     votes_id AS fact_votes_id,
-    sysdate() AS inserted_timestamp,
-    sysdate() AS modified_timestamp
+    SYSDATE() AS inserted_timestamp,
+    SYSDATE() AS modified_timestamp
 FROM
     {{ ref('silver__votes') }}
 WHERE
     block_timestamp IS NOT NULL
+
 {% if is_incremental() %}
-    AND modified_timestamp > '{{ max_modified_timestamp }}'
+AND modified_timestamp > '{{ max_modified_timestamp }}'
 {% endif %}
