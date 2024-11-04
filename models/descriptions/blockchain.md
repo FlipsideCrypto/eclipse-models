b9a8b81ac37317097c704bf0697762d0f9ee918d
tarikceric <tarik@flipsidecrypto.com>
2024-11-04 15:49:06 -0800

    add dim_labels

---
+++ models/descriptions/blockchain.md
@@ -0,0 +1,5 @@
+{% docs blockchain %}
+
+In this table, always Eclipse. Used to join to cross-chain tables. 
+
+{% enddocs %}
---
+++ models/gold/core/core__dim_labels.sql
@@ -0,0 +1,18 @@
+{{ config(
+    materialized = 'view',
+    tags = ['scheduled_non_core']
+) }}
+
+SELECT
+    blockchain,
+    creator,
+    address,
+    label_type,
+    label_subtype,
+    project_name AS label,
+    address_name AS address_name,
+    labels_combined_id AS dim_labels_id,
+    inserted_timestamp,
+    modified_timestamp
+FROM
+    {{ ref('silver__labels') }}
