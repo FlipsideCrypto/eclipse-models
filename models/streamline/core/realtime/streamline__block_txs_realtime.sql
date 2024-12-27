8502a71fc8e4c1ffb0515de002be348044acafec
shah <info@shahnewazkhan.ca>
2024-12-27 05:31:55 -0800

    revert worker_batch_size -> 20k

--- models/streamline/core/realtime/streamline__block_txs_realtime.sql
+++ models/streamline/core/realtime/streamline__block_txs_realtime.sql
@@ -7,7 +7,7 @@
         params ={ "external_table" :"block_txs",
         "sql_limit" :"100000",
         "producer_batch_size" :"100000",
-        "worker_batch_size" :"10000",
+        "worker_batch_size" :"20000",
         "sql_source" :"{{this.identifier}}",
         "order_by_column": "block_id",
         "exploded_key": tojson(["result.transactions"]) }
